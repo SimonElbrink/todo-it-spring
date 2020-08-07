a10094526137e302ce490d24b95b897cf358c14d
Simon Elbrink <Simon.Elbrink@gmail.com>
2020-08-07 11:45:21 +0200

    update

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO app_user (id, first_name, last_name) VALUES
+  ('Example-User-1','Simon', 'Elbrink'),
+  ('Example-User-2', 'Ulf', 'Bengtsson'),
+  ('Example-User-3','Fredrik', 'Odin'),
+  ('Example-User-4', 'Mehrdad', 'Javan');
+
+INSERT INTO todo_item(id, title, description, deadline, done, assignee_id) VALUES
+    ('Example-TodoItem-1', 'Finish Assignment!','The assignment is to create TodoItems.', '2020-01-01 17:00:00', false, 'Example-User-1'),
+    ('Example-TodoItem-2', 'Create More Assignments!','Spawn new ideas with teammates.', '2020-01-02 8:00:00', false, 'Example-User-3'),
+    ('Example-TodoItem-3', 'Create More Exercises!','Spawn new ideas with teammates.', '2020-01-03 8:00:00', false, 'Example-User-3');
